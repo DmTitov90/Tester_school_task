ea491ccd5db0b982a0e6cfb90e63ab05b405ae97
di-ti <di-tits@yandex.ru>
2022-02-19 11:36:59 +0300

    add Task_1 Task_2 19.02.2022

--- src/com/test_3/Test_3.java
+++ src/com/test_3/Test_3.java
@@ -4,20 +4,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+
 /**
  * Creator Dmitrii Titov
  * Date 19.02.2022 Version 1.0.0
  */
 public class Test_3 {
     static int count = 0;
-    static int[] arr = new int[]{2, -1, 2, 0, 4, 3};
+    static int[] arr = new int[]{2, -1, 2, 0, 1, 3};
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
         System.out.print("Введите число циклопов: ");
         int cyclCount = Integer.parseInt(reader.readLine());
-
         Set<Integer> set = new HashSet<>();
         List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(arr);
@@ -36,9 +35,9 @@ public class Test_3 {
             }
         }
         if (count * 2 == cyclCount - 1) {
-            System.out.println(++count);
-        } else {
             System.out.println(count);
+        } else {
+            System.out.println(count++);
         }
     }
 }
