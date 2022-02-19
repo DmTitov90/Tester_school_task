05e2b1429ec33ed1101f5718fe1aa03627f7a8d6
di-ti <di-tits@yandex.ru>
2022-02-19 22:37:21 +0300

    add Task_1 Task_2 19.02.2022

--- src/com/test_1/Number.java
+++ src/com/test_1/Number.java
@@ -3,7 +3,7 @@ package com.test_1;
 class Number {
     private int userNumber;
     private int sum;
-    private boolean check;
+    private boolean check = true;
 
     public Number(String userNumber) {
         this.userNumber = Integer.parseInt(userNumber);
@@ -14,14 +14,16 @@ class Number {
     }
 
     private int checker() {
-        for (int primeNumber = 1; primeNumber < userNumber; primeNumber++) {
+        for (int primeNumber = 2; primeNumber < userNumber; primeNumber++) {
             for (int i = 2; i < primeNumber; i++) {
                 if (primeNumber % i == 0) {
+                    check = false;
                     break;
                 }
             }
-            if (check) sum = sum + primeNumber;
-            else check = true;
+            if (check) {
+                sum = sum + primeNumber;
+            } else check = true;
         }
         return sum;
     }
