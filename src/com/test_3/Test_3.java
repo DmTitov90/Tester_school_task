6767015f1be2ffd50e55b8a2433a648d04cb5c1e
di-ti <di-tits@yandex.ru>
2022-02-19 11:09:19 +0300

    add Task_1 Task_2 19.02.2022

--- src/com/test_3/Test_3.java
+++ src/com/test_3/Test_3.java
@@ -4,22 +4,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-
+/**
+ * Creator Dmitrii Titov
+ * Date 19.02.2022 Version 1.0.0
+ */
 public class Test_3 {
     static int count = 0;
-    static int[] arr = new int[]{2, -1, 2, 0, 6, 3};
+    static int[] arr = new int[]{2, -1, 2, 0, 4, 3};
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
         System.out.print("Введите число циклопов: ");
         int cyclCount = Integer.parseInt(reader.readLine());
-
-        int[] alist = new int[10];
-        for (int i = 0; i < alist.length; i++)
-        {
-            String s = reader.readLine();
-            alist[i] = Integer.parseInt(s);
-        }
 
         Set<Integer> set = new HashSet<>();
         List<List<Integer>> list = new ArrayList<>();
@@ -27,7 +24,6 @@ public class Test_3 {
         for (int i : arr) {
             list.add(new ArrayList<>(Arrays.asList(i - 1, i, i + 1)));
         }
-
         for (List<Integer> integers : list) {
             for (Integer integer : integers) {
                 if (set.contains(integer)) {
@@ -39,7 +35,6 @@ public class Test_3 {
                 }
             }
         }
-
         if (count * 2 == cyclCount - 1) {
             System.out.println(++count);
         } else {
