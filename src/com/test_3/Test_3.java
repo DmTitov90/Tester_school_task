b3f2fd6ff3dd0ecb415d465c1a40e19528fd7dda
di-ti <di-tits@yandex.ru>
2022-02-19 23:14:04 +0300

    add Task_1 Task_2 19.02.2022

--- src/com/test_3/Test_3.java
+++ src/com/test_3/Test_3.java
@@ -1,8 +1,6 @@
 package com.test_3;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
 
 /**
@@ -10,31 +8,37 @@ import java.util.*;
  * Date 19.02.2022 Version 1.0.0
  */
 public class Test_3 {
+    static int cyclCount = 5;
     static int count = 0;
-    static int[] arr = new int[]{2, -1, 2, 0, 1, 3};
+    static int[] arr = new int[]{2, -1, 2, 0, 1};
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Введите число циклопов: ");
-        int cyclCount = Integer.parseInt(reader.readLine());
+    public static void main(String[] args) {
+        //создаем Множество(Set) чтобы проверять дубликаты
         Set<Integer> set = new HashSet<>();
+        //Создаем массив массивов в которую вносим линзу и рекомендуемый диапазон
         List<List<Integer>> list = new ArrayList<>();
+        //Сортируем линзы
         Arrays.sort(arr);
+        //Добавляем диапазоны допустимых линз для каждого циклопа
         for (int i : arr) {
             list.add(new ArrayList<>(Arrays.asList(i - 1, i, i + 1)));
         }
         for (List<Integer> integers : list) {
+            //Проверяем соседние дубликаты диапазонов
             for (Integer integer : integers) {
                 if (set.contains(integer)) {
+                    //В случае дубликата, добавляем линзу для пары циклопов
                     count++;
+                    //Сет мы обновляем чтобы отсечь проверенные пары
                     set.clear();
+                    // Останавливаем цикл для перехода на следующего циклопа
                     break;
                 } else {
                     set.add(integer);
                 }
             }
         }
-        System.out.println(count);
+        // Проверка, если кол-во циклопов не четное, тогда добавляем еще одну пару линз
         if (count * 2 == cyclCount - 1) {
             System.out.println(++count);
         } else {
